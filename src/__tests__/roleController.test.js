b526d81f5f46f8c38a41184f9c84c362369076f4
Guilherme Peixoto <guizinpeix7@gmail.com>
2025-01-29 23:44:43 -0300

    tests fixed

--- src/__tests__/roleController.test.js
+++ src/__tests__/roleController.test.js
@@ -176,245 +176,67 @@ describe("RoleController Test Suite", () => {
         roleId = response.body._id;
     });
 
-    //   it('should get all roles', async () => {
-    //     const response = await request(app)
-    //       .get('/role')
-    //       .set('Authorization', `Bearer ${authToken}`);
+    it("should get all roles", async () => {
+        const response = await request(app)
+            .get("/role")
+            .set("Authorization", `Bearer ${authToken}`);
 
-    //     expect(response.status).toBe(200);
-    //     expect(Array.isArray(response.body)).toBe(true);
-    //   });
+        expect(response.status).toBe(200);
+        expect(Array.isArray(response.body)).toBe(true);
+    });
 
-    //   it('should get role by ID', async () => {
-    //     const response = await request(app)
-    //       .get(`/role/${roleId}`)
-    //       .set('Authorization', `Bearer ${authToken}`);
+    it("should get role by ID", async () => {
+        const response = await request(app)
+            .get(`/role/${roleId}`)
+            .set("Authorization", `Bearer ${authToken}`);
 
-    //     expect(response.status).toBe(200);
-    //     expect(response.body).toHaveProperty('_id', roleId);
-    //   });
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty("_id", roleId);
+    });
 
-    //   it('should update a role by ID', async () => {
-    //     const updatedData = { name: 'SuperAdmin' };
+    it("should update a role by ID", async () => {
+        const updatedData = { name: "SuperAdmin" };
 
-    //     const response = await request(app)
-    //       .put(`/role/${roleId}`)
-    //       .set('Authorization', `Bearer ${authToken}`)
-    //       .send(updatedData);
+        const response = await request(app)
+            .patch(`/role/patch/${roleId}`)
+            .set("Authorization", `Bearer ${authToken}`)
+            .send(updatedData);
 
-    //     expect(response.status).toBe(200);
-    //     expect(response.body.name).toBe('SuperAdmin');
-    //   });
+        expect(response.status).toBe(200);
+        expect(response.body.name).toBe("SuperAdmin");
+    });
 
-    //   it('should assign permissions to a role', async () => {
-    //     // Criar uma permissão de teste
-    //     const permission = await Permission.create({ name: 'Test Permission' });
+    it("should assign permissions to a role", async () => {
+        // Criar uma permissão de teste
+        const permission = await Permission.create({ name: "Test Permission" });
 
-    //     const response = await request(app)
-    //       .post(`/role/${roleId}/permissions`)
-    //       .set('Authorization', `Bearer ${authToken}`)
-    //       .send({ permissions: [permission._id] });
+        const response = await request(app)
+            .put(`/roles/${roleId}/permissions`)
+            .set("Authorization", `Bearer ${authToken}`)
+            .send({ permissions: [permission._id] });
 
-    //     expect(response.status).toBe(200);
-    //     expect(response.body.role.permissions).toContain(permission._id.toString());
-    //   });
+        expect(response.status).toBe(200);
+        expect(response.body.role.permissions).toContain(
+            permission._id.toString()
+        );
+    });
 
-    //   it('should delete a role by ID', async () => {
-    //     const response = await request(app)
-    //       .delete(`/role/${roleId}`)
-    //       .set('Authorization', `Bearer ${authToken}`);
+    it("should delete a role by ID", async () => {
+        const response = await request(app)
+            .delete(`/role/delete/${roleId}`)
+            .set("Authorization", `Bearer ${authToken}`);
 
-    //     expect(response.status).toBe(204);
-    //   });
+        expect(response.status).toBe(204);
+    });
 
-    //   it('should not delete a protected role', async () => {
-    //     // Criar role protegida
-    //     const protectedRole = await Role.create({ name: 'Protected', isProtected: true });
+    it("should handle role not found", async () => {
+        const fakeId = new mongoose.Types.ObjectId();
 
-    //     const response = await request(app)
-    //       .delete(`/role/${protectedRole._id}`)
-    //       .set('Authorization', `Bearer ${authToken}`);
+        const response = await request(app)
+            .get(`/role/${fakeId}`)
+            .set("Authorization", `Bearer ${authToken}`);
 
-    //     expect(response.status).toBe(403);
-    //     expect(response.body.message).toBe('Cannot delete protected role');
-    //   });
-
-    //   it('should handle role not found', async () => {
-    //     const fakeId = new mongoose.Types.ObjectId();
-
-    //     const response = await request(app)
-    //       .get(`/role/${fakeId}`)
-    //       .set('Authorization', `Bearer ${authToken}`);
-
-    //     expect(response.status).toBe(404);
-    //     expect(response.body.message).toBe('Role not found');
-    //   });
-
-    // describe("POST /role/create", () => {
-    //     it("should create a new role", async () => {
-    //         const newRole = generateRoleData("001");
-    //         const response = await createRole(newRole);
-    //         expect(response.status).toBe(201);
-    //         expect(response.body).toHaveProperty("_id");
-    //     });
-
-    //     it("should not create a new role with no name", async () => {
-    //         const newRole = {
-    //             permissions: [
-    //                 {
-    //                     module: "finance",
-    //                     access: ["create"],
-    //                 },
-    //                 {
-    //                     module: "benefits",
-    //                     access: ["create"],
-    //                 },
-    //                 {
-    //                     module: "users",
-    //                     access: ["create"],
-    //                 },
-    //             ],
-    //         };
-    //         const response = await createRole(newRole);
-    //         expect(response.status).toBe(400);
-    //     });
-    // });
-
-    // describe("GET /role", () => {
-    //     it("should retrieve all roles", async () => {
-    //         const response = await request(app).get("/role").set("Authorization", `Bearer ${authToken}`);
-    //         expect(response.status).toBe(200);
-    //         expect(response.body).toBeInstanceOf(Array);
-    //     });
-    // });
-
-    // describe("GET /role/:id", () => {
-    //     it("should retrieve a specific role by ID", async () => {
-    //         //Create Role
-    //         const newRole = generateRoleData("003");
-    //         const postResponse = await createRole(newRole);
-    //         expect(postResponse.status).toBe(201);
-    //         expect(postResponse.body).toHaveProperty("_id");
-
-    //         //Get ID and try access
-    //         const roleId = postResponse.body._id;
-    //         const getResponse = await request(app).get(`/role/${roleId}`).set("Authorization", `Bearer ${authToken}`);
-
-    //         expect(getResponse.status).toBe(200);
-    //         expect(getResponse.body).toHaveProperty("_id", roleId);
-    //     });
-
-    //     it("should not retrieve a specific role with invalid ID", async () => {
-    //         //Try access with invalid ID
-    //         const roleId = "A1";
-    //         const getResponse = await request(app).get(`/role/${roleId}`).set("Authorization", `Bearer ${authToken}`);
-
-    //         expect(getResponse.status).toBe(500);
-    //     });
-    // });
-
-    // describe("PATCH /role/patch/:id", () => {
-    //     it("should update the role data", async () => {
-    //         //Create Role
-    //         const roleData = generateRoleData("007");
-    //         const postResponse = await createRole(roleData);
-    //         expect(postResponse.status).toBe(201);
-
-    //         const roleId = postResponse.body._id;
-    //         const updatedData = {
-    //             name: "Perfil Updated",
-    //         };
-
-    //         const response = await request(app)
-    //             .patch(`/role/patch/${roleId}`)
-    //             .send(updatedData);
-
-    //         expect(response.status).toBe(200);
-    //         expect(response.body).toHaveProperty("_id", roleId);
-    //         expect(response.body.name).toBe("Perfil Updated");
-    //     });
-
-    //     it("should not update the role data with invalid ID", async () => {
-    //         const roleId = "A1";
-    //         const updatedData = {
-    //             name: "Perfil Updated",
-    //         };
-
-    //         const response = await request(app)
-    //             .patch(`/role/patch/${roleId}`)
-    //             .set("Authorization", `Bearer ${authToken}`)
-    //             .send(updatedData);
-
-    //         expect(response.status).toBe(400);
-    //     });
-    // });
-
-    // // Novos testes adicionados para deletar role protegida
-    // describe("DELETE /role/:id", () => {
-    //     it("should delete a specific role", async () => {
-    //         const newRole = generateRoleData("004");
-    //         const postResponse = await createRole(newRole);
-    //         expect(postResponse.status).toBe(201);
-    //         const roleId = postResponse.body._id;
-
-    //         const deleteResponse = await request(app).delete(
-    //             `/role/delete/${roleId}`
-    //         ).set("Authorization", `Bearer ${authToken}`);
-    //         expect(deleteResponse.status).toBe(204);
-    //     });
-
-    //     it("should not delete a specific role with invalid ID", async () => {
-    //         const roleId = "A1";
-
-    //         const deleteResponse = await request(app).delete(
-    //             `/role/delete/${roleId}`
-    //         ).set("Authorization", `Bearer ${authToken}`);
-    //         expect(deleteResponse.status).toBe(500);
-    //     });
-
-    //     it("should not delete a protected role", async () => {
-    //         // Cria uma nova role protegida
-    //         const protectedRoleData = {
-    //             name: "Role Protegida",
-    //             permissions: [
-    //                 { module: "finance", access: ["read"] },
-    //                 { module: "benefits", access: ["read"] },
-    //             ],
-    //             isProtected: true,
-    //         };
-    //         const postResponse = await createRole(protectedRoleData);
-    //         expect(postResponse.status).toBe(201);
-    //         const roleId = postResponse.body._id;
-
-    //         // Tenta deletar a role protegida
-    //         const deleteResponse = await request(app).delete(
-    //             `/role/delete/${roleId}`
-    //         ).set("Authorization", `Bearer ${authToken}`);
-    //         expect(deleteResponse.status).toBe(403);
-    //         expect(deleteResponse.body.message).toBe(
-    //             "Cannot delete protected role"
-    //         );
-    //     });
-
-    //     it("should delete a non-protected role", async () => {
-    //         // Cria uma nova role não protegida
-    //         const nonProtectedRoleData = {
-    //             name: "Role Não Protegida",
-    //             permissions: [
-    //                 { module: "users", access: ["read"] },
-    //                 { module: "benefits", access: ["read"] },
-    //             ],
-    //             isProtected: false,
-    //         };
-    //         const postResponse = await createRole(nonProtectedRoleData);
-    //         expect(postResponse.status).toBe(201);
-    //         const roleId = postResponse.body._id;
-
-    //         // Tenta deletar a role não protegida
-    //         const deleteResponse = await request(app).delete(
-    //             `/role/delete/${roleId}`
-    //         ).set("Authorization", `Bearer ${authToken}`);
-    //         expect(deleteResponse.status).toBe(204);
-    //     });
-    // });
+        expect(response.status).toBe(404);
+        expect(response.body.message).toBe("Role not found");
+    });
 });
